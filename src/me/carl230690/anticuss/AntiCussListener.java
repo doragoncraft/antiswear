055bb024c6cb6386951a822786bd957d3ea6f4bf
doragoncraft <carlcraftmc@gmail.com>
2013-10-13 06:11:25 +0100

    CLOWN FIX ITTTTTTTTTT

--- src/me/carl230690/anticuss/AntiCussListener.java
+++ src/me/carl230690/anticuss/AntiCussListener.java
@@ -1,32 +1,46 @@
-/*    */ package me.carl230690.anticuss;
-/*    */ 
-/*    */ import java.io.PrintStream;
-/*    */ import java.util.List;
-/*    */ import net.milkbowl.vault.economy.Economy;
-/*    */ import net.milkbowl.vault.economy.EconomyResponse;
-/*    */ import org.bukkit.ChatColor;
-/*    */ import org.bukkit.configuration.file.FileConfiguration;
-/*    */ import org.bukkit.entity.Player;
-/*    */ import org.bukkit.event.EventHandler;
-/*    */ import org.bukkit.event.Listener;
-/*    */ import org.bukkit.event.player.AsyncPlayerChatEvent;
-/*    */ 
-/*    */ public class AntiCussListener
-/*    */   implements Listener
-/*    */ {
-/*    */   public AntiCuss plugin;
-/*    */ 
-/*    */   public AntiCussListener(AntiCuss instance)
-/*    */   {
-/* 16 */     this.plugin = instance;
-/*    */   }
-/*    */ 
-/*    */   @EventHandler
-/*    */   public void onPlayerChat(AsyncPlayerChatEvent e) {
+ package me.carl230690.anticuss;
+ 
+ import java.io.PrintStream;
+import java.util.List;
+
+ import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
+ 
+ public class AntiCussListener{
+   implements Listener, Plugin;
+ {
+   public AntiCuss plugin;
+ 
+   public AntiCussListener(AntiCuss instance)
+   {
+     this.plugin = instance;
+   }
+   int maxCapsPercentage;
+   int minLength;
+   static String upperCase;
+   public void onEnable()
+  {
+    Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    saveDefaultConfig();
+    this.maxCapsPercentage = getConfig().getInt("max-caps-percentage");
+    this.minLength = getConfig().getInt("min-message-length");
+    upperCase = getConfig().getString("uppercase-characters");
+  }
+   @EventHandler
+   public void onPlayerChat(AsyncPlayerChatEvent e) {
 /* 21 */     for (String word : e.getMessage().toLowerCase().split(" "))
 /* 22 */       if ((e.getPlayer().hasPermission("anticuss.bypass")) || (e.getPlayer().isOp())) {
 /* 23 */         e.setCancelled(false);
-/*    */       }
+       }
 /* 25 */       else if ((!e.getPlayer().hasPermission("anticuss.kick")) && (this.plugin.getConfig().getStringList("cusswords").contains(word))) {
 /* 26 */         e.setCancelled(true);
 /* 27 */         String warnmessage = this.plugin.getConfig().getString("warnmessage");
@@ -35,43 +49,40 @@
 /* 30 */           e.getPlayer().setHealth(0);
 /* 31 */           String killMsg = this.plugin.getConfig().getString("killMessage");
 /* 32 */           e.getPlayer().sendMessage(ChatColor.RED + killMsg);
-/*    */         }
+         }
 /* 34 */         int cost = this.plugin.getConfig().getInt("cost");
 /* 35 */         if (cost > 0) {
 /* 36 */           EconomyResponse cussr = AntiCuss.econ.withdrawPlayer(e.getPlayer().getName(), cost);
 /* 37 */           if (cussr.transactionSuccess())
 /* 38 */             e.getPlayer().sendMessage(ChatColor.RED + "You were charged $" + cost + " for cussing!");
-/*    */           else
+           else
 /* 40 */             e.getPlayer().sendMessage(ChatColor.RED + "An error occurred!");
-/*    */         }
-/*    */       }
-/*    */   }
-/*    */ 
-/*    */   @EventHandler
-/*    */   public void onAsyncPlayerChat(AsyncPlayerChatEvent e)
-/*    */   {
-/* 53 */     Player player = e.getPlayer();
-/* 54 */     String message = e.getMessage();
-/* 55 */     if (!player.hasPermission("anticuss.bypass")) {
-/* 56 */       int caps = 0;
-/* 57 */       for (int i = 0; i < message.length(); i++) {
-/* 58 */         if (Character.isUpperCase(message.charAt(i)))
-/* 59 */           caps++;
-/*    */       }
-/*    */       try
-/*    */       {
-/* 63 */         int iPercent = this.plugin.getConfig().getInt("capsPercentage");
-/*    */ 
-/* 65 */         if (1.0D * caps / message.length() * 100.0D > iPercent) {
-/* 66 */           e.setCancelled(true);
-/* 67 */           player.sendMessage(ChatColor.RED + "You used too many caps in that chat message.");
-/*    */         }
-/*    */       } catch (ArithmeticException ae) {
-/* 70 */         System.out.println("Message had a length of 0 :(");
-/*    */       }
-/*    */     }
-/*    */   }
-/*    */ }
+         }
+       }
+   }
+ 
+     @EventHandler
+  public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+    if ((!event.getPlayer().hasPermission("anticuss.caps.bypass")) && 
+      (event.getMessage().length() >= this.minLength) && 
+      (getUppercasePercentage(event.getMessage()) > this.maxCapsPercentage))
+      event.setMessage(event.getMessage().toLowerCase());
+  }
+
+  public static boolean isUppercase(String string)
+  {
+    return upperCase.contains(string);
+  }
+
+  public static double getUppercasePercentage(String string) {
+    double upperCase = 0.0D;
+    for (int i = 0; i < string.length(); i++) {
+      if (isUppercase(string.substring(i, i + 1))) {
+        upperCase += 1.0D;
+      }
+    }
+    return upperCase / string.length() * 100.0D;
+  }}
 
 /* Location:           C:\Users\carl\Desktop\AntiCuss.jar
  * Qualified Name:     me.carl230690.anticuss.AntiCussListener
--- src/me/carl230690/anticuss/commands/Clear.java
+++ src/me/carl230690/anticuss/commands/Clear.java
@@ -24,13 +24,13 @@ import org.bukkit.Bukkit;
        sender.sendMessage(ChatColor.RED + "You must be a player to execute this command!");
      } else {
        Player p = (Player)sender;
-       if ((cmd.getName().equalsIgnoreCase("clearchat")) && 
-         (args.length == 0)) {
+       if ((cmd.getName().equalsIgnoreCase("clearchat")||((cmd.getName().equalsIgnoreCase("ctc")) && 
+         (args.length == 0)))) {
          if (!p.hasPermission("anticuss.clear"))
            p.sendMessage(ChatColor.RED + "You do not have permission to execute this command!");
          else if (p.hasPermission("anticuss.clear")) {
            for (int msgs = 1; msgs <= 100; msgs++) {
-             p.sendMessage("");}
+        	 Bukkit.broadcastMessage("");}
           {  Bukkit.broadcastMessage(ChatColor.AQUA + "Chat has been cleared by " + sender.getName());
            }
          }
